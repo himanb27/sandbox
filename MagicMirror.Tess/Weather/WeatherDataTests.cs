f381cb34c8ec7402e90faf61af7cfb181ee6aec0
Himan Baroi <himanb27@gmail.com>
2018-07-22 14:36:25 -0400

    test

---
+++ MagicMirror.Tess/Weather/WeatherDataTests.cs
@@ -0,0 +1,31 @@
+﻿using MagicMirror.DataAccess.Repos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MagicMirror.Tess.Weather
+{
+    public class WeatherDataTests
+    {
+        private WeatherRepo _repo;
+
+        public WeatherDataTests()
+        {
+            //test
+            _repo = new WeatherRepo();
+        }
+
+        [Fact]
+        public void Can_Retrive_Weather_Data();
+        {
+            // Arrange 
+            WeatherEntity entity = null;
+            string city = "London";
+            
+            //Act
+            entity = await _repo
+
+        }
+    }
+}
